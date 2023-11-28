ba9aa3b2e9a18cac7901a58f8b27d264b2884c2a
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2023-11-27 16:14:56 -0800

    100-write.py

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * check_cycle - function to check if the linked list has cylcle
+ * @list: the linked list
+ * Return: if there is no cyle, 0, otherwise 1
+ */
+int check_cycle(listint_t *list)
+{
+
