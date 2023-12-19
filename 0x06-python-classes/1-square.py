712a0b3b2b5b01c8d9132b302481d8c18f3507c4
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2023-12-19 09:22:54 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square class."""
+
+
+class Square:
+    """Square definition."""
+
+    def __init__(my_square, size):
+        """constructor.
+
+        Arguments:
+            size: side of square length.
+        """
+        my_square.__size= size
