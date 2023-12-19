2fc0d162ddaefb0846fff39ea747e86a8d032f36
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2023-12-19 13:57:01 -0800

    103-magic_class.py

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Magic class to print pytcodes."""
+
+import math
+
+
+class MagicClass:
+    """circle class."""
+
+    def __init__(self, radius=0):
+        """MagicClass initialization.
+        Args:
+        radius (float or int): Radius of MagicClas.
+        """
+        self.__radius = 0
+        if type(radius) is not int and type (radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """calculate the area of MagicClass."""
+        return (self.__radius ** 2 * math.pi)
+
+    def circumference(self):
+        """calculates the circumference of MagicClass."""
+        return (2 * math.pi * self._radius)
