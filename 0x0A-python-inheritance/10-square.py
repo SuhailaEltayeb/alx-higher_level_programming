8697461c4a9f82af0a82a5c9ff11bb660422af57
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-01-08 12:11:56 -0800

    10-main.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Rectangle class module"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """rectangle subclass"""
+    def __init__(self, size):
+        """Constructor."""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """square area function"""
+        return self.__size ** 2
