1baf195b9a3b1e0f858e5d1955125a303471fb39
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-01-08 07:50:13 -0800

    3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Is kind of class method module"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Checks ig an object is subclass of a class"""
+    return isinstance(obj, a_class)
