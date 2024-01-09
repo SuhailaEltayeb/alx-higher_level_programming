aaaa850da80e1729055288792e7b8b23f9f8e9a1
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-01-09 10:26:17 -0800

    10-main.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Student class definition."""
+
+
+class Student:
+    """student representation."""
+
+    def __init__(self, first_name, last_name, age):
+        """new Student initialization.
+
+        Args:
+            first_name (str): student 1st name.
+            last_name (str): student last name.
+            age (int): student age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return student dictionary representation.
+
+        If attrs are list of strings, represents only attrs
+        included in list.
+
+        Args:
+            attrs (list): (Optional) The attributes be represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
