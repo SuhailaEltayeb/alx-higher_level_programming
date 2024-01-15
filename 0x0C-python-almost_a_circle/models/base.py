a0321d0bb18a2020ace77731c2f146fba2941e9b
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-01-15 05:31:51 -0800

    models/base.py, models/__init__.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Base class module"""
+
+
+class Base:
+    """Base class representation."""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """constructor."""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
