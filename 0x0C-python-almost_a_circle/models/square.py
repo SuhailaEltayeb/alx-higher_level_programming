f7782aee75522b744979d3870629b93298f25b19
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-01-15 10:03:46 -0800

    Square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -24,3 +24,21 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def __update(self, id=None, size=None, x=None, y=None):
+        """function to ssign attributes to class instance."""
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """function to ssign attributes to class instance using *args/**kwargs"""
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
