3af565f0c92468748e49e70a21cef52a08d41e78
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-02-13 10:33:34 -0800

    11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
--- Script to lists *  records with score >= 10 in second_table
+-- Script to lists records, score >= 10 in second_table
 SELECT score, name FROM second_name WHERE score >= 10
 ORDER BY score DESC;
