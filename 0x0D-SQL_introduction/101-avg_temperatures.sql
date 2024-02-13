eefebf7d36dc96342c500f6b0a3c7ad51e38695f
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-02-13 11:17:55 -0800

    101-avg_temperatures.sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,4 @@
 -- Script to displays the average temperature by city
-SELECT city, AVG(value) AS avg_temp from temperatures
+SELECT city, AVG(value) AS avg_temp FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;
