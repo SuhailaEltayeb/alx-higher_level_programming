505e9182c5aa087f364102777c02de4f28d61ead
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-02-14 06:44:26 -0800

    2-create_read_user.sql

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Script to creates database hbtn_0d_2 & user user_0d_2
+-- & grant read privilege to user
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
