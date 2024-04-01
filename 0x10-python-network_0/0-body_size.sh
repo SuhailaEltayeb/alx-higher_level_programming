a72d347ddbb8160e88ed0413e762eb4d87e38509
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-04-01 13:23:03 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#Script to takes a URL, sends request to that URL, 
-# display response body size
+#Script to take URL,send request to that URL,display response body size
 curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
